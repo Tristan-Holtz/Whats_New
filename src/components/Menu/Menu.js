88a6631832fd448801fb9a89e56ca01705e702df
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-01-30 08:56:52 -0700

    Create menu component

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,26 @@
 import React from 'react';
-import './Menu.css'
+import './Menu.css';
 
-// MENU COMPONENT CODE GOES HERE
+const Menu = ({
+  localNews,
+  technologyNews,
+  entertainmentNews,
+  scienceNews,
+  healthNews,
+  changeCategory
+}) => {
+  return (
+    <aside className="menu">
+      <h2>Whats New</h2>
+      <button onClick={() => changeCategory(localNews)}>Local News</button>
+      <button onClick={() => changeCategory(technologyNews)}>Technology</button>
+      <button onClick={() => changeCategory(entertainmentNews)}>
+        Entertainment
+      </button>
+      <button onClick={() => changeCategory(scienceNews)}>Science</button>
+      <button onClick={() => changeCategory(healthNews)}>Health</button>
+    </aside>
+  );
+};
 
 export default Menu;
