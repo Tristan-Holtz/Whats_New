32c7542497cecce53fcdb681d4a32b55eab25a7d
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-01-30 12:38:53 -0700

    Create search bar

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './SearchForm.css';
 
-// SEARCHFORM COMPONENT CODE GOES HERE
+const SearchForm = props => {
+  return (
+    <nav className="nav">
+      <input className="nav-search"></input>
+      <button onclick={props.runSearch}>SEARCH</button>
+    </nav>
+  );
+};
 
 export default SearchForm;
